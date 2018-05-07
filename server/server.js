cd8562325161dffb8093093e325c60b3d390d5fd
Jacob <jerling1989@gmail.com>
2018-05-07 02:32:29 -0400

    testing express app

--- server/server.js
+++ server/server.js
@@ -3,8 +3,21 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.status(404).send({
+		error: 'Page Not Found.',
+		name: 'Todo App v1.0'
+	});
+});
+
+
+app.get('/users', (req, res) => {
+	res.send([
+		{name: 'Jacob', age: 28},
+		{name: 'Andrew', age: 24},
+		{name: 'Jordan', age: 35}
+	]);
 });
 
 
 app.listen(3000);
+module.exports.app = app;
